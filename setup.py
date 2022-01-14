a3f376827202413767b15dce94bb370df807549f
radwinskis <markradwin@gmail.com>
2022-01-14 08:44:20 -0700

    v0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'MARK RADWIN',  
   author_email = 'markradwin@gmail.com',     
   url = 'https://github.com/radwinskis/RSAbs',  
-  download_url = 'https://github.com/radwinskis/RSAbs/archive/refs/tags/v0.4.tar.gz',   
+  download_url = 'https://github.com/radwinskis/RSAbs/archive/refs/tags/v0.5.tar.gz',   
   keywords = ['REFLECTANCE', 'SPECTROSCOPY', 'ABSORPTION', 'DETECTION'],  
   install_requires=[          
           'scipy',
@@ -17,7 +17,7 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
-    'Intended Audience :: Scientists',      
+    'Intended Audience :: Science/Research',      
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
@@ -28,6 +28,7 @@ setup(
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11'
     
   ],
 )
