3b304a267bc6ac925aec044d01bb3e2cf85f9f93
radwinskis <markradwin@gmail.com>
2022-01-20 12:07:12 -0700

    v0.8.1

--- RSAbs/__init__.py
+++ RSAbs/__init__.py
@@ -1,2 +1,4 @@
 from .absorptions import *
 from .seek_absorptions import *
+from .seek_absorptions_depth import *
+from .absorptions_depth import *
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'RSAbs',       
   packages = ['RSAbs'],   
-  version = '0.8', 
+  version = '0.8.1', 
   license='MIT',  
   description = 'AUTOMATIC DETECTION OF ABSORPTION FEATURES IN REFLECTANCE SPECTRA', 
   author = 'MARK RADWIN',  
   author_email = 'markradwin@gmail.com',     
   url = 'https://github.com/radwinskis/RSAbs',  
-  download_url = 'https://github.com/radwinskis/RSAbs/archive/refs/tags/v0.8.tar.gz',   
+  download_url = 'https://github.com/radwinskis/RSAbs/archive/refs/tags/v0.8.1.tar.gz',   
   keywords = ['REFLECTANCE', 'SPECTROSCOPY', 'ABSORPTION', 'DETECTION'],  
   install_requires=[          
           'scipy',
